717122c7827e0db2e67f15d0eb26b9c5637411e9
iftekher2108 <parvezkhan21082002@gmail.com>
2023-10-04 18:48:12 +0600

    mejor fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,60 @@
-console.log('iftekher mahmud pervez')
+// console.log('iftekher mahmud pervez')
+
+
+// education popup show
+
+let education =document.querySelector("form .education"),
+    add_btn =document.querySelector("form button");
+
+// console.log(add_btn);
 
 
 
-// confrimed email popup show
 
-let select_email = document.querySelector('#email'),
-    re_email = document.querySelector('.email');
+// education popup show
 
-    console.log(re_email)
 
-    select_email.oninput = () => {
-        if (select_email.value.includes('@gmail.') || select_email.value.includes('@yahoo.') || select_email.value.includes('@outlook.') || select_email.value.includes('@aol.') || select_email.value.includes('@protonmail.') || select_email.value.includes('@icloud.') || select_email.value.includes('@yandex.') || select_email.value.includes('@zoho.') || select_email.value.includes('@gmx.')) {
-            
-            setTimeout(() => {
-              re_email.classList.remove('hidden');
-            }, 500);
+
+
+// section 2 study time auto pickup
+
+let start_date = document.querySelector("#start-date"),
+    end_date = document.querySelector("#end-date"),
+    durations = document.querySelector("#study-time");
+
+
+
+function change() {  
+
+        const start_data = new Date(start_date.value)
+       const start_year = start_data.getFullYear()
     
-        } else {
-            re_email.classList.add('hidden');
+        const end_data = new Date(end_date.value)
+       const end_year = end_data.getFullYear()
+
+        durations.value = end_year - start_year +' '+ "years"
+
+}
+
+// section 2 study time auto pickup
+
+
+class car {
+    constructor( name,roll) {
+        this.name = name;
+        this.roll = roll;
     
-        }
     }
-
-// confrimed email popup show
-
+}
 
 
+mycar1 = new car("iftekher",200)
+
+mycar2 = new car("mahmud",400)
+
+console.log(mycar1,mycar2)
+
+  
 
 
 
